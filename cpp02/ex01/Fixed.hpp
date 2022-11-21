f8ca525cdc930f9995ade16e8853971bb27690ab
Ciro García Belmonte <cigarcia@student.42malaga.com>
2022-11-21 06:24:14 +0100

    cpp02/ex01

---
+++ cpp02/ex01/Fixed.hpp
@@ -0,0 +1,32 @@
+
+#include <iostream>
+#include <cmath>
+#include <string>
+
+class Fixed {
+
+private:
+	int num;
+	static const int fract_bits = 16;
+
+	float getDecimalPart() const;
+	float getWholePart() const;
+
+	void setWholePart(uint32_t n);
+	void setDecimalPart(uint32_t n);
+
+public:
+	Fixed();
+	Fixed(const int32_t);
+	Fixed(const float);
+	Fixed(const Fixed &other);
+	Fixed operator=(const Fixed& other);
+	~Fixed();
+
+	float toFloat() const;
+	int toInt() const;
+	int getRawBits() const;
+	void setRawBits(int const raw);
+
+	friend std::ostream& operator<<(std::ostream& os, const Fixed& f);
+};
