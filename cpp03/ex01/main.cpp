c7ab1d80929098b3a649d870deca662351c5f1da
Ciro García Belmonte <cigarcia@student.42malaga.com>
2022-11-24 19:37:49 +0100

    Renamed variables

--- cpp03/ex01/main.cpp
+++ cpp03/ex01/main.cpp
@@ -3,22 +3,22 @@
 
 int main(void) {
 
-	ScavTrap claptrap1;
-	ScavTrap claptrap2("ScavTrap2");
-	ScavTrap claptrap3(claptrap2);
+	ScavTrap scavtrap1;
+	ScavTrap scavtrap2("ScavTrap2");
+	ScavTrap scavtrap3(scavtrap2);
 
-	claptrap1.attack("ScavTrap2");
-	claptrap2.takeDamage(5);
-	claptrap3.beRepaired(1);
-	claptrap2.takeDamage(6);
-	claptrap2.beRepaired(1);
-	claptrap2.attack("ScavTrap1");
+	scavtrap1.attack("ScavTrap2");
+	scavtrap2.takeDamage(5);
+	scavtrap3.beRepaired(1);
+	scavtrap2.takeDamage(6);
+	scavtrap2.beRepaired(1);
+	scavtrap2.attack("ScavTrap1");
 	for (int i = 0; i < 50; i++)
-		claptrap1.attack("ScavTrap1");
+		scavtrap1.attack("ScavTrap1");
 	
-	claptrap1.guardGate();
-	claptrap2.takeDamage(500);
-	claptrap2.beRepaired(50);
+	scavtrap1.guardGate();
+	scavtrap2.takeDamage(500);
+	scavtrap2.beRepaired(50);
 
 	return (0);
 }
