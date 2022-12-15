1e5ffb5fd2925921ed767870c4b956016b59b0b3
Ciro García Belmonte <cigarcia@student.42malaga.com>
2022-12-15 11:47:11 +0100

    changes

--- cpp04/ex02/main.cpp
+++ cpp04/ex02/main.cpp
@@ -7,8 +7,16 @@
 int main(void) {
 
 	Cat cat;
+	Dog dog;
+
+	// Won't compile
+	//Animal animal;
 
 	cat.makeSound();
+	dog.makeSound();
+
+	//Even if it did compile, this wouldn't work anyways
+	//animal.makeSound();
 
 	return (0);
 }
