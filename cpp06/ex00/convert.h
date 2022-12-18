eeb4f1575d1fab5ea42b4a78f9a8b1530aa0f847
Ciro García Belmonte <ciro.garcia.belmonte@gmail.com>
2022-12-18 09:33:03 +0100

    cpp06/ex00

---
+++ cpp06/ex00/convert.h
@@ -0,0 +1,9 @@
+
+#ifndef SCALAR_H
+# define SCALAR_H
+
+# include <iostream>
+# include <string>
+
+
+#endif
---
+++ cpp06/ex00/main.cpp
@@ -0,0 +1,13 @@
+
+
+#include "convert.h"
+
+int main(int argc, char **argv)
+{
+	if (argc != 2)
+	{
+		std::cout << "Error: Wrong number of arguments" << std::endl;
+		return (1);
+	}
+	return (0);
+}
